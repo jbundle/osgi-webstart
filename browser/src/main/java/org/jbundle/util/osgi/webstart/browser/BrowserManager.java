fd5bdaf38a0e81cd994d6c6a2b2bc4f08b2b631c
Don Corley <don@donandann.com>
2012-12-05 10:40:49 -0800

    Add doji and java to javascript code

---
+++ browser/src/main/java/org/jbundle/util/osgi/webstart/browser/BrowserManager.java
@@ -0,0 +1,131 @@
+/*
+ * MuffinManager.java
+ *
+ * Created on January 30, 2001, 12:14 AM
+ 
+ * Copyright © 2012 jbundle.org. All rights reserved.
+ */
+package org.jbundle.util.osgi.webstart.browser;
+
+import java.applet.Applet;
+import java.util.HashMap;
+import java.util.Map;
+
+import netscape.javascript.JSObject;
+
+import org.jbundle.util.osgi.webstart.util.UrlUtil;
+
+/** 
+ * BrowserManager - This code handles the browser interface for the java web start program.
+ * Note: These utilities have no external dependencies so potential users can
+ * include them in their osgi-webstart javascript wrapped code.
+ * @author  Don Corley don@tourgeek.com
+ * @version 1.0.0
+ */
+public class BrowserManager extends Object
+{
+	private Applet m_applet = null;
+	
+	protected Map<String,Object> m_propertiesInitialCommand = null;
+	
+    public static final String BLANK = "";  // Blank String
+
+    /**
+     * Creates new BrowserManager.
+     */
+    public BrowserManager()
+    {
+        super();
+    }
+    /**
+     * Creates new BrowserManager.
+     * @param applet The applet object.
+     * @param strInitialCommand The initial command params
+     */
+    public BrowserManager(Applet applet, Map<String,Object> mapInitialCommands)
+    {
+        this();
+        this.init(applet, mapInitialCommands);
+    }
+    /**
+     * Creates new MuffinManager.
+     * @param applet The applet object.
+     */
+    public void init(Applet applet, Map<String,Object> mapInitialCommands)
+    {
+    	m_applet = applet;
+		m_propertiesInitialCommand = mapInitialCommands;
+    }
+    /**
+     * Push this command onto the browser's history stack.
+     * @param command
+     */
+	public void pushBrowserHistory(String command, String title)
+	{
+        String args[] = new String[2];
+        args[0] = this.getCommandForBrowser(command);
+    	if (title == null)
+    		title = BLANK;
+        args[1] = title;
+        this.callJavascript("pushBrowserHistory", args);
+	}
+	/**
+	 * Given this command, return the full command in an array of 1.
+	 * @param command
+	 * @return
+	 */
+	public String getCommandForBrowser(String command)
+	{
+        if ((command != null) || (m_propertiesInitialCommand != null))
+        {
+    		Map<String,Object> properties = new HashMap<String,Object>();
+    		if (m_propertiesInitialCommand != null)
+    			properties.putAll(m_propertiesInitialCommand);
+        	if (command != null)
+        		UrlUtil.parseArgs(properties, command);	// Note - duplicates will go with this version
+    		command = BLANK;
+    		for (String key : properties.keySet())
+    		{
+    			if (("samewindow".equalsIgnoreCase(key))
+        			|| ("navmenus".equalsIgnoreCase(key))
+    				|| ("menubars".equalsIgnoreCase(key)))
+    					continue;
+    			if (properties.get(key) == null)
+    				properties.put(key, BLANK);
+    			command = UrlUtil.addURLParam(command, key, properties.get(key).toString());
+    		}
+        }
+        return command;
+	}
+	/**
+	 * Pop some commands from the browser's history.
+	 * Note: The browser will NOT call your java doJavaBrowserBack method, you need to do the navigation yourself.
+	 * Note: If you pop more commands than you have on the stack, the browser will back up
+	 * into it's html page history and display the target web page.
+	 * @param commandsToPop The number of commands to pop from the history.
+	 */
+	public void popBrowserHistory(int commandsToPop, boolean bCommandHandledByJava, String title)
+	{
+        String args[] = new String[3];
+    	args [0] = Integer.toString(commandsToPop);
+    	args [1] = Boolean.toString(bCommandHandledByJava);
+    	if (title == null)
+    		title = BLANK;
+        args [2] = title;
+        this.callJavascript("popBrowserHistory", args);
+	}
+	/**
+	 * Call javascript with this command.
+	 * @param command
+	 * @param args
+	 */
+	public void callJavascript(String command, String args[])
+	{
+		try {
+	        JSObject win = JSObject.getWindow(m_applet);
+	        win.call(command, args);
+		} catch (Exception ex) {
+			// Ignore any errors
+		}		
+	}
+}
---
+++ util/src/main/java/org/jbundle/util/osgi/webstart/util/UrlUtil.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright © 2012 jbundle.org. All rights reserved.
+ */
+package org.jbundle.util.osgi.webstart.util;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Basic utilities.
+ * Note: These utilities have no external dependencies so potential users can
+ * include them in their osgi-webstart javascript wrapped code.
+ */
+public class UrlUtil extends Object
+{	
+    public static final String BLANK = "";  // Blank String
+    public final static String URL_ENCODING = "UTF-8";
+
+    /**
+     * Add this param and data to this URL.
+     * @param strOldURL The original URL to add this param to.
+     * @param strParam The parameter to add.
+     * @param strData The data this parameter is set to.
+     * @return The new URL string.
+     */
+    public static String addURLParam(String strOldURL, String strParam, String strData)
+    {
+        return UrlUtil.addURLParam(strOldURL, strParam, strData, true);
+    }
+    /**
+     * Add this param and data to this URL.
+     * @param strOldURL The original URL to add this param to.
+     * @param strParam The parameter to add.
+     * @param strData The data this parameter is set to.
+     * @param bAddIfNull Add an empty param if the data is null?
+     * @return The new URL string.
+     */
+    public static String addURLParam(String strOldURL, String strParam, String strData, boolean bAddIfNull)
+    {
+        String strURL = strOldURL;
+        if ((strOldURL == null) || (strOldURL.length() == 0))
+            strURL = "?";
+        else if (strOldURL.indexOf('?') == -1)
+            strURL += "?";
+        else
+            strURL += "&";
+        if (strData == null)
+        {
+            if (!bAddIfNull)
+                return strOldURL; // Don't add a null param.
+            strData = BLANK;
+        }
+        try {
+            strURL += URLEncoder.encode(strParam, URL_ENCODING) + '=' + URLEncoder.encode(strData, URL_ENCODING);
+        } catch (java.io.UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+        }
+        return strURL;
+    }
+    /**
+     * Parse this URL formatted string into properties.
+     * @properties The properties object to add the params to.
+     * @args The arguments to parse (each formatted as key=value).
+     */
+    public static Map<String,Object> parseArgs(Map<String,Object> properties, String[] args)
+    {
+        if (args == null)
+            return properties;
+        if (properties == null)
+        	properties = new HashMap<String,Object>();
+        for (int i = 0; i < args.length; i++)
+            UrlUtil.addParam(properties, args[i], false);
+        return properties;
+    }
+    /**
+     * Parse this URL formatted string into properties.
+     * @properties The properties object to add the params to.
+     * @args The URL to parse (formatted as: XYZ?key1=value1&key2=value2).
+     */
+    public static Map<String,Object> parseArgs(Map<String,Object> properties, String strURL)
+    {
+        int iIndex = 0;
+        int iStartIndex = strURL.indexOf('?') + 1;  // Start of first param (0 if no ?)
+        while ((iIndex = strURL.indexOf('=', iIndex)) != -1)
+        {
+            int iEndIndex = strURL.indexOf('&', iIndex);
+            if (iEndIndex == -1)
+                iEndIndex = strURL.length();
+            if (iStartIndex < iEndIndex)
+                UrlUtil.addParam(properties, strURL.substring(iStartIndex, iEndIndex), true);
+            iStartIndex = iEndIndex + 1;
+            iIndex++;
+        }
+        return properties;
+    }
+    /**
+     * Parse the param line and add it to this properties object.
+     * (ie., key=value).
+     * @properties The properties object to add this params to.
+     * @param strParam param line in the format param=value
+     */
+    public static void addParam(Map<String,Object> properties, String strParams, boolean bDecodeString)
+    {
+        int iIndex = strParams.indexOf('=');
+        int iEndIndex = strParams.length();
+        if (iIndex != -1)
+        {
+            String strParam = strParams.substring(0, iIndex);
+            String strValue = strParams.substring(iIndex + 1, iEndIndex);
+            if (bDecodeString)
+            {
+                try {
+                    strParam = URLDecoder.decode(strParam, URL_ENCODING);
+                    strValue = URLDecoder.decode(strValue, URL_ENCODING);
+                } catch (java.io.UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            properties.put(strParam, strValue);
+        }
+    }
+}
